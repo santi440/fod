3f7c08c7792223174a0575ab7207e3a1ca842718
Santi Marcos <santiago0105@hotmail.com.ar>
2024-04-11 18:54:16 -0300

    orden de carpetas

--- ej4/ej4p2.pas
+++ ej4/ej4p2.pas
@@ -33,30 +33,71 @@ begin
 	else min:= a2; leer(det2,a2);
 end;
 
+procedure crearArchivos(var f1,f2,f3:Text; var d1,d2 : detalle; var m1:archivo);
+var 
+	p:provincia;
+	c:censo;
+begin
+	reset(f1);
+	rewrite(m1);
+	while(not EoF(f1))do
+	begin
+		readln(f1, p.alfabetizados , p.encuestados , p.nombre);
+		write(m1,p);
+	end;
+	reset(f2);
+	rewrite(d1);
+	while(not EoF(f2))do
+	begin
+		readln(f2,c.localidad, c.alfabetizados , c.encuestados , c.nombre);
+		write(d1,c);
+	end;
+	reset(f3);
+	rewrite(d2);
+	while(not EoF(f3))do
+	begin
+		readln(f3,c.localidad, c.alfabetizados , c.encuestados , c.nombre);
+		write(d2,c);
+	end;
+	Close(f1);
+	Close(f2);
+	Close(f3);
+end;
+
 var
 	agencia1,agencia2: detalle;
 	maestro: archivo;
+	fuente1,fuente2,fuente3: Text;
 	d1,d2,min: censo;
 	m1: provincia;
 begin
 	Assign(agencia1,'detalle1');
 	Assign(agencia2,'detalle2');
 	Assign(maestro,'alfabetizados');
+	Assign(fuente1,'alfabetizados.txt');
+	Assign(fuente2,'det.txt');
+	Assign(fuente3,'det2.txt');
+	
+	crearArchivos(fuente1,fuente2,fuente3,agencia1,agencia2,maestro);
+	
 	Reset(agencia1);
 	Reset(agencia2);
 	Reset(maestro);
+	
 	leer(agencia1,d1);
 	leer(agencia2,d2);
 	read(maestro,m1);
+	
 	minimo(min,d1,d2,agencia1,agencia2);
+	
 	while(min.nombre<>valorAlto)do
 	begin
 		while(m1.nombre <> min.nombre)do
 			read(maestro,m1);
 		while(m1.nombre = min.nombre)do
 		begin
-			m1.alfabetizados:= m1.alfabetizados + d1.alfabetizados;
-			m1.encuestados:= m1.encuestados + d1.encuestados;
+			m1.alfabetizados:= m1.alfabetizados + min.alfabetizados;
+			m1.encuestados:= m1.encuestados + min.encuestados;
 			minimo(min,d1,d2,agencia1,agencia2);
 		end;
 		seek(maestro,filepos(maestro)-1);
